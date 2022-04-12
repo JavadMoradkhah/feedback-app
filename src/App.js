ecf9848ae27863b7578e473f4df9bd1ce577cd9f
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-04-12 16:13:37 +0430

    Adding global state functions to the context.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
-import FeedbackData from './data/FeedbackData';
 import AboutPage from './pages/AboutPage';
 import AboutLink from './components/AboutLink';
 import { FeedbackProvider } from './context/FeedbackContext';
 import './css/index.css';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-
   return (
     <FeedbackProvider>
       <Router>
@@ -36,9 +20,9 @@ function App() {
               index
               element={
                 <>
-                  <FeedbackForm handleAdd={addFeedback} />
-                  <FeedbackStats feedback={feedback} />
-                  <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
                 </>
               }
             />
