4aa043e5ab3a348372f986ee7e344ed5190a35e1
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-04-12 14:30:38 +0430

    Checking feedback array is empty in FeedbackList component.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,6 +2,10 @@ import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
 function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
