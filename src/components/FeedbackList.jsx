d7a27c202c78891038ec81b4c5ab596c7a9c73bc
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-04-12 12:46:08 +0430

    Implementing feedbacks deleting logic.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,11 @@
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
-function FeedbackList({ feedback }) {
-  if (!feedback || feedback.length === 0) {
-    return <p>No Feedback Yet!</p>;
-  }
+function FeedbackList({ feedback, handleDelete }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
